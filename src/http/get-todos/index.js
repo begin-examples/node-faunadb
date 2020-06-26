e430e510c123567508b3c75b5b1b5dc6da72c9c2
ShawnHosea <shawnhosea@shawnhosea.com>
2020-06-26 03:42:16 -0400

    new

---
+++ src/http/get-todos/index.js
@@ -0,0 +1,32 @@
+let begin = require('@architect/functions')
+
+let faunadb = require('faunadb'),
+q = faunadb.query
+
+let client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+})
+
+
+// TODO change defaultJSON
+let defaultJSON = {
+  ok: true
+}
+
+function route (req, res) {
+  console.log(JSON.stringify(req, null, 2))
+
+  client.query({a:"Hello from FaunaDB"}).then((faunaResult)=> {
+    console.log(client.query)
+    res({
+      json: faunaResult
+    })
+  }).catch((error) => {
+    console.log("backend error", error)
+    res({
+      json: defaultJSON
+    })
+  })
+}
+
+exports.handler = begin.json.get(route)
