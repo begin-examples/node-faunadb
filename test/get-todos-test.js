6bba6ce81b66a38656ed820d36a4fd8b3e12dced
ShawnHosea <shawnhosea@shawnhosea.com>
2020-06-28 19:00:12 -0400

    add sandbox

--- test/get-todos-test.js
+++ test/get-todos-test.js
@@ -1,22 +1,22 @@
-// let tiny = require('tiny-json-http')
-// let test = require('tape')
-// let sandbox = require('@architect/sandbox')
-// let end
+let tiny = require('tiny-json-http')
+let test = require('tape')
+let sandbox = require('@architect/sandbox')
+let end
 
-// // this starts a sandbox environment for the tests to excecute in.
-// test('start', async t => {
-//   t.plan(1)
-//   end = await sandbox.start()
-//   t.ok(true, 'started')
-// })
+// this starts a sandbox environment for the tests to excecute in.
+test('start', async t => {
+  t.plan(1)
+  end = await sandbox.start()
+  t.ok(true, 'started')
+})
 
-// // test case will go here
+// test case will go here
 
 
 
-// // this ends sandbox
-// test('end', t => {
-//   t.plan(1)
-//   end()
-//   t.ok(true, 'ended')
-// })
+// this ends sandbox
+test('end', t => {
+  t.plan(1)
+  end()
+  t.ok(true, 'ended')
+})
